e3a7fedddce11d517323fa1d598e194a5fca7140
iamzhuravlevtimofey <dudarev.sasha2018@gmail.com>
2022-06-28 00:57:27 +0500

    first project

---
+++ lab2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число А");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число B");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Режимы: Сложение +, вычитание -, умножение *, деление /");
+            string X = Convert.ToString(Console.ReadLine());
+            string Plus;
+            string Minus;
+            string Umnoj;
+            string Razdel;
+
+            if (X == "+")
+            {
+                int C = A + B;
+                Console.WriteLine("Ответ: " + C);
+            }
+            if (X == "-")
+            {
+                int C = A - B;
+                Console.WriteLine("Ответ: " + C);
+            }
+            if (X == "*")
+            {
+                int C = A * B;
+                Console.WriteLine("Ответ: " + C);
+            }
+            if (X == "/")
+            {
+                int C = A / B;
+                Console.WriteLine("Ответ: " + C);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
